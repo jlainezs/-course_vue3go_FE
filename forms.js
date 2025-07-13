d70d05ee398620fdac071effe231a661fd6a6ebd
Pep Lainez <jlainezs@gmail.com>
2025-07-13 19:21:45 +0200

    2-21 conditional rendering.

--- forms.js
+++ forms.js
@@ -41,10 +41,10 @@ const SelectInput = {
 }
 
 const CheckInput = {
-  props: ["label", "required", "name", "value"],
+  props: ["label", "required", "name", "value", "checked"],
   template: `
     <div class="mb-3">
-        <input class="form-check-input" type="checkbox" :required="required" :value="value" :id="name">
+        <input class="form-check-input" type="checkbox" :checked="checked" :required="required" :value="value" :id="name">
         <label class="form-check-label" :for="name">{{ label }}</label>
     </div>`
 }
