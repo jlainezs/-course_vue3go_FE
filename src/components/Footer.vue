74c3fa0c995266679c9a257e2fb89c517dced0a6
Pep Lainez <jlainezs@gmail.com>
2025-07-14 08:09:03 +0200

    2-34 Add content to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,17 @@
 <template>
-  Footer
+  <footer class="d-flex align-items-center justify-content-center mt-5">
+    Copyright &copy; {{ new Date().getFullYear() }}
+  </footer>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+footer {
+  background-color: black;
+  color: white;
+  min-height: 3em;
+  font-size: 0.8em;
+}
 </style>
