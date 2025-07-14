d358e811b1b76bedf7eebd30e4085b29fd0f53fe
Pep Lainez <jlainezs@gmail.com>
2025-07-14 08:05:33 +0200

    2-33 Add content to body

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,5 +1,11 @@
 <template>
-  Body
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img alt="Book and glasses" src="./../assets/book-and-glasses.png" class="img-fluid mt-5"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
