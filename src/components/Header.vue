240a38b1e942076dd08a07984d46c7466f293071
Pep Lainez <jlainezs@gmail.com>
2025-07-19 22:44:43 +0200

    7-84 Logging out.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
           </li>
           <li class="nav-item">
             <router-link v-if="store.token === ''" class="nav-link" to="/login">Login</router-link>
-            <router-link v-else class="nav-link" to="/logout">Logout</router-link>
+            <a v-else class="nav-link" href="javascript:void(0);" @click="logout">Logout</a>
           </li>
         </ul>
       </div>
@@ -22,11 +22,19 @@
 
 <script>
 import { store } from "./store.js"
+import router from "./../router/index.js"
+
 export default {
   name: "header",
   data() {
     return {
       store,
+    }
+  },
+  methods: {
+    logout() {
+      store.token = "";
+      router.push("/login");
     }
   }
 }
