4f2cf026f443b306c5f79b49ee61ef68d2e02432
Pep Lainez <jlainezs@gmail.com>
2025-07-20 12:33:36 +0200

    8-92 Setting up stub components and routes.

---
+++ src/components/Books.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-3">Books</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
