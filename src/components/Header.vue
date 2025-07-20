50c9df72c5da45626cad9493132b2bd966f0b53b
Pep Lainez <jlainezs@gmail.com>
2025-07-20 10:16:37 +0200

    7-88 Finishing up the improved login process.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
           </li>
@@ -15,6 +15,11 @@
             <a v-else class="nav-link" href="javascript:void(0);" @click="logout">Logout</a>
           </li>
         </ul>
+
+        <span class="navbar-text">
+          {{ store.user.first_name ?? "" }}
+        </span>
+
       </div>
     </div>
   </nav>
@@ -47,6 +52,13 @@ export default {
           console.log(response.message)
         } else {
           store.token = "";
+          store.user = {};
+
+          // remove cookie
+          document.cookie = "_site_data=; Path=/; " +
+            "SameSite=Strict; Secure; " +
+            "Expires=Thu, 01 Jan 1970 00:00:01 CMT;";
+
           router.push("/login");
         }
       })
