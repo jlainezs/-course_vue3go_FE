c6c949a4867697cbbdd5dc4486b4ea05fca622dd
Pep Lainez <jlainezs@gmail.com>
2025-07-24 22:19:19 +0200

    8-116 Checking status of token on every request
    Add token validation logic and integrate it into router navigation guards.

--- src/components/security.js
+++ src/components/security.js
@@ -20,6 +20,36 @@ let Security = {
       body: JSON.stringify(payload),
       headers: headers
     }
+  },
+
+  checkToken: function () {
+    if (store.token !== "") {
+      const payload = {
+        token: store.token,
+      }
+      const headers = new Headers();
+      headers.append("Content-type", "application/json");
+      let requestOptions = {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: headers,
+      }
+      fetch(import.meta.env.VITE_API_URL + "/validate-token", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          if (!data.data){
+            store.token = "";
+            store.user = {};
+            document.cookie = "_site_data=; Path=/; "
+              + "SameSite=Strict; Secure; "
+              + "Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+          }
+        }
+      });
+    }
   }
 }
 
