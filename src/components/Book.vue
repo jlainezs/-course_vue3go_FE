a43992c02c6603bbf342f87ee4314c3dbf752207
Pep Lainez <jlainezs@gmail.com>
2025-07-27 09:59:56 +0200

    9-124 Displaying individual books
    Expand the `Book` component to display detailed information from the API. Update `Books` component to include router links for navigating to individual book pages.

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,9 +1,44 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col">
-        <h1 class="mt-3">Book</h1>
+      <div class="col-md-2">
+        <img class="img-fluid img-thumbnail" :src="`${imgPath}/covers/${book.slug}.jpg`" alt="cover for {{book.title}}"/>
+      </div>
+      <div class="col-md-10">
+        <h3 class="mt-3">{{ book.title }}</h3>
+        <p>
+          <strong>Author:</strong> {{ book.author.author_name }}<br/>
+          <strong>Published</strong> {{ book.publication_year }}<br/>
+        </p>
+        <p>
+          {{ book.description }}
+        </p>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import Security from "@/components/security.js";
+
+export default {
+  data(){
+    return {
+      book: {},
+      imgPath: import.meta.env.VITE_API_IMAGE_URL,
+    }
+  },
+  created(){
+    fetch(import.meta.env.VITE_API_URL + "/books/" + this.$route.params.bookName)
+    .then(response => response.json())
+    .then((data) => {
+      if (data.error) {
+        this.$emit('error', data.message);
+      } else {
+        this.book = data.data;
+        console.log(this.book.title);
+      }
+    })
+  }
+}
+</script>
