595d3372ef43a8f0d86d5c8127e5008465c36208
Pep Lainez <jlainezs@gmail.com>
2025-07-27 20:33:50 +0200

    9-124 Saving the state of Books component.
    Introduce a loading state to `Book` component, ensuring UI feedback while API data is fetched. Wrap dynamic components in `keep-alive` to preserve state across navigation.

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -2,17 +2,20 @@
   <div class="container">
     <div class="row">
       <div class="col-md-2">
-        <img class="img-fluid img-thumbnail" :src="`${imgPath}/covers/${book.slug}.jpg`" alt="cover for {{book.title}}"/>
+        <img v-if="this.ready" class="img-fluid img-thumbnail" :src="`${imgPath}/covers/${book.slug}.jpg`" alt="cover for {{book.title}}"/>
       </div>
       <div class="col-md-10">
-        <h3 class="mt-3">{{ book.title }}</h3>
-        <p>
-          <strong>Author:</strong> {{ book.author.author_name }}<br/>
-          <strong>Published</strong> {{ book.publication_year }}<br/>
-        </p>
-        <p>
-          {{ book.description }}
-        </p>
+        <template v-if="this.ready">
+          <h3 class="mt-3">{{ book.title }}</h3>
+          <p>
+            <strong>Author:</strong> {{ book.author.author_name }}<br/>
+            <strong>Published</strong> {{ book.publication_year }}<br/>
+          </p>
+          <p>
+            {{ book.description }}
+          </p>
+        </template>
+        <p v-else>Loading...</p>
       </div>
     </div>
   </div>
@@ -26,19 +29,23 @@ export default {
     return {
       book: {},
       imgPath: import.meta.env.VITE_API_IMAGE_URL,
+      ready: false,
     }
   },
-  created(){
+  activated() {
     fetch(import.meta.env.VITE_API_URL + "/books/" + this.$route.params.bookName)
-    .then(response => response.json())
-    .then((data) => {
-      if (data.error) {
-        this.$emit('error', data.message);
-      } else {
-        this.book = data.data;
-        console.log(this.book.title);
-      }
-    })
+      .then(response => response.json())
+      .then((data) => {
+        if (data.error) {
+          this.$emit('error', data.message);
+        } else {
+          this.book = data.data;
+          this.ready = true;
+          console.log(this.book.title);
+        }
+      })
+  },
+  deactivated() {
   }
 }
 </script>
